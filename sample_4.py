ed18432998964ab2afd39ca081597ed8118217fa
CedricAnover <cedric.anover@hotmail.com>
2024-12-28 20:52:45 +0800

    refactor(iteration-4): minor update in sample 4

--- sample_4.py
+++ sample_4.py
@@ -20,14 +20,12 @@ def my_callback(node: Node, dep_results: dict[str, Result], message=None, use_pr
 
 
 if __name__ == "__main__":
-    # Source Nodes
-    node_1 = Node("1", my_callback)
-    node_2 = Node("2", my_callback, message="Hello World")
-
     # Construct a Dag with DagBuilder
     dag_builder = DagBuilder()
-    dag_builder.add_node("3", my_callback, depends_on=[node_1, node_2])
-    dag_builder.add_node("4", my_callback, depends_on=["3", node_1], message="Foo Bar")
+    dag_builder.add_node("1", my_callback)
+    dag_builder.add_node("2", my_callback)
+    dag_builder.add_node("3", my_callback, depends_on=["1", "2"])
+    dag_builder.add_node("4", my_callback, depends_on=["3", "1"], message="Foo Bar")
     dag_builder.add_node("5", my_callback, depends_on=["3"])
     dag_builder.add_node("6", my_callback, depends_on=["5"], message="Hello World")
     dag_builder.add_node("7", my_callback, depends_on=["4", "6"])
